80c7c8cfa13ea88a9c80bb3ac807214c15e00e34
dlwlzjatmznf-cmyk <dlwlzjatmznf@gmail.com>
2026-06-11 15:07:25 +0900

    survey.js

--- js/survey.js
+++ js/survey.js
@@ -20,9 +20,6 @@ document.getElementById("surveyForm").addEventListener("submit", async function(
 
         const response = await fetch(SCRIPT_URL,{
             method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
             body:JSON.stringify(formData)
         });
 
