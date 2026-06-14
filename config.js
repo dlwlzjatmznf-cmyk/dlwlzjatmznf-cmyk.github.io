19c86035e6033c1a93c96ca5e3855dda5c3235a3
dlwlzjatmznf-cmyk <dlwlzjatmznf@gmail.com>
2026-06-14 10:07:34 +0900

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 // [수요조사 마스터 스위치] 평소(꺼짐)에는 false, 수요조사 기간(켬)에는 true를 적으세요.
 const IS_SURVEY_OPEN = false;
+// 혹은 true
+
+// ★ 방금 만든 구글 공지사항 시트 ID를 입력하세요
+const NOTICE_SHEET_ID = "https://docs.google.com/spreadsheets/d/1ipQcNeLpNi93oAgEdRaXvWE7AkX3qP99nqx7uFwx_W8/edit?gid=0#gid=0";
